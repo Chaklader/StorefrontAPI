96ffb6c890dd257a7f86d2eb732e1bcbb2888d5e
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2021-08-16 21:42:14 +0600

    added dummy test for product handler

---
+++ src/handlers/tests/products_spec.ts
@@ -0,0 +1,23 @@
+import { Product } from '../../models/products';
+
+
+/* 
+## Product data scheme
+
+export type Product = {
+    id?: number;
+    name: string;
+    price: number;
+    category?: string;
+};
+*/
+describe('Product Handler', () => {
+
+    /* 
+      check if the methods in the product handlers are functioning
+      as expected
+    */
+    it('create request with playload should return correct response', async () => {
+
+    });
+});
